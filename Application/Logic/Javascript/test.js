a2301b1effd7a6a1c3ad974ba9754551091d0446
JustinJmnz <JJimenez@my.bridgeport.edu>
2016-11-25 14:05:58 -0500

    Initial Commit

---
+++ Application/Logic/Javascript/test.js
@@ -0,0 +1,13 @@
+﻿if (annyang) {
+    // Let's define a command.
+    var commands = {
+        'hello': function () { alert('Hello world!'); },
+        'show me a map': function() { alert('showing map'); }
+    };
+
+    // Add our commands to annyang
+    annyang.addCommands(commands);
+
+    // Start listening.
+    annyang.start();
+}
