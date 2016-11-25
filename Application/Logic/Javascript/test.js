363d7ea520688ae0d8be62780dd98a8caf14d4c4
JustinJmnz <JJimenez@my.bridgeport.edu>
2016-11-25 14:37:04 -0500

    Created Structure

--- Application/Logic/Javascript/test.js
+++ Application/Logic/Javascript/test.js
@@ -1,13 +1,31 @@
-﻿if (annyang) {
-    // Let's define a command.
-    var commands = {
-        'hello': function () { alert('Hello world!'); },
-        'show me a map': function() { alert('showing map'); }
-    };
+﻿$(function () { // This method fires when the DOM (Document Object Model) is loaded
+    startClock();
+    startListening();
+});
 
-    // Add our commands to annyang
-    annyang.addCommands(commands);
+// Will start the STT (Speech to Text)
+function startListening() {
+    if (annyang) {
+        // Add more Commands here
+        var commands = {
+            'hello': function () { alert('Hello world!'); },
+            'show me a map': function () { alert('showing map'); }
+        };
+        // For more complex commands visit https://www.talater.com/annyang/
 
-    // Start listening.
-    annyang.start();
+        // Add our commands to annyang
+        annyang.addCommands(commands);
+
+        // Start listening.
+        annyang.start();
+    }
 }
+// Will start the clock, refreshing every second
+function startClock() {
+    setInterval(function () {
+        $('#time').html(new Date().toLocaleTimeString());
+    }, 1000);
+}
+
+
+
