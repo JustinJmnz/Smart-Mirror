ac88ce524204e6485687ea3591c5a49ded740a68
JustinJmnz <jjimenez@my.bridgeport.edu>
2017-01-11 22:06:17 -0500

    Added timer and layout of website

--- Application/JavaScript/home.js
+++ Application/JavaScript/home.js
@@ -1,26 +1,34 @@
-﻿$(function () {
+
+
+$(function () {
+    var mainContainer = $('#main-container');
+    var section1 = $('#section-1');
+    var section2 = $('#section-2');
+    var section3 = $('#section-3');
+
     startListening();
-    setInterval(function () {
-        $('#time').html(new Date().toLocaleTimeString());
-    }, 1000);
-});
+    section3.append(getTimeDiv());
 
-function startListening() {
-    if (annyang) {
-        // Let's define a command.
-        var commands = {
-            'hello': function () { alert('Hello world!'); }
-        };
+    function startListening() {
+        if (annyang) {
+            // Let's define a command.
+            var commands = {
+                'hello': function () { alert('Hello world!'); }
+            };
 
-        // Add our commands to annyang
-        annyang.addCommands(commands);
-        //https://www.talater.com/annyang/ for more examples
-        // Start listening.
-        annyang.start();
+            // Add our commands to annyang
+            annyang.addCommands(commands);
+            //https://www.talater.com/annyang/ for more examples
+            // Start listening.
+            annyang.start();
+        }
     }
-}
-function getTime() {
-    var $timeDiv = $('<div></div>');
-    $timeDiv.append('<p id="time"></p>');
-    return $timeDiv;
-}
+    function getTimeDiv() {
+        var timeDiv = $('<div></div>');
+        timeDiv.append('<p id="time"></p>');
+        setInterval(function() {
+            timeDiv.html(new Date().toLocaleTimeString());
+        }, 1000);
+        return timeDiv;
+    }
+});
