9b829fb84d7531e947e8d83bd41a7f4498477e9c
justinjmnz <jjimenez@my.bridgeport.edu>
2017-01-11 20:07:14 -0500

    adding everything

---
+++ Application/JavaScript/home.js
@@ -0,0 +1,26 @@
+﻿$(function () {
+    startListening();
+    setInterval(function () {
+        $('#time').html(new Date().toLocaleTimeString());
+    }, 1000);
+});
+
+function startListening() {
+    if (annyang) {
+        // Let's define a command.
+        var commands = {
+            'hello': function () { alert('Hello world!'); }
+        };
+
+        // Add our commands to annyang
+        annyang.addCommands(commands);
+        //https://www.talater.com/annyang/ for more examples
+        // Start listening.
+        annyang.start();
+    }
+}
+function getTime() {
+    var $timeDiv = $('<div></div>');
+    $timeDiv.append('<p id="time"></p>');
+    return $timeDiv;
+}
