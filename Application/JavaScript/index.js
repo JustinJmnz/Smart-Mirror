20ffd7acaf85def9934b5a808737f0d6327f9c4e
JustinJmnz <jjimenez@my.bridgeport.edu>
2017-01-23 18:58:08 -0500

    Finalized layout and added widget focus

--- Application/JavaScript/index.js
+++ Application/JavaScript/index.js
@@ -4,15 +4,71 @@ $(function () { // Main Function
     startListening();
 
     var mainContainer = $('#main-container');
+    // Top Section Variables
     var sectionTop = $('#section-top');
-    var sectionMiddle = $('#section-middle');
+    var widgetOne = sectionTop.children().eq(0); // Check the 0th child of sectionTop Div
+    var widgetTwo = sectionTop.children().eq(1);
+    var widgetThree = sectionTop.children().eq(2);
+    var widgetFour = sectionTop.children().eq(3);
+    // Bottom Section Variables
     var sectionBottom = $('#section-bottom');
-    var paneLeft = $('#pane-left');
-    var paneMiddle = $('#pane-middle');
-    var paneRight = $('#pane-right');
+    var hub = $('#hub');
 
-    sectionMiddle.append(getTimeDiv());
+    setInterval(function() {
+        focusWidget(1);
+    }, 1000);
+    setInterval(function() {
+        focusWidget(2);
+    }, 1100);
+    setInterval(function() {
+        focusWidget(3);
+    }, 1200);
+    setInterval(function() {
+        focusWidget(4);
+    }, 1300);
 
+    hub.append(getTimeDiv());
+
+
+    // Focuses or Unfocuses on a widget depending on number
+    function focusWidget(number) {
+        switch (number) {
+            case 1:
+                var widgetImg = widgetOne.children().first();
+                if (widgetImg.hasClass('bump-up')){
+                    widgetImg.removeClass('bump-up');
+                }else {
+                    widgetImg.addClass('bump-up');
+                }
+                break;
+            case 2:
+                var widgetImg = widgetTwo.children().first();
+                if (widgetImg.hasClass('bump-up')){
+                    widgetImg.removeClass('bump-up');
+                }else {
+                    widgetImg.addClass('bump-up');
+                }
+                break;
+            case 3:
+                var widgetImg = widgetThree.children().first();
+                if (widgetImg.hasClass('bump-up')){
+                    widgetImg.removeClass('bump-up');
+                }else {
+                    widgetImg.addClass('bump-up');
+                }
+                break;
+            case 4:
+                var widgetImg = widgetFour.children().first();
+                if (widgetImg.hasClass('bump-up')){
+                    widgetImg.removeClass('bump-up');
+                }else {
+                    widgetImg.addClass('bump-up');
+                }
+                break;
+            default:
+                console.log("focusWidget() -> default case");
+        }
+    }
     // Creates a div with the time in it
     function getTimeDiv() {
         console.log('Creating time divs');
