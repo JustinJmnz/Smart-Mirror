2c2932ca794ab42a4979e08d6c301983f61b55ed
JustinJmnz <jjimenez@my.bridgeport.edu>
2017-02-09 22:30:42 -0500

    Added news funtionality

--- Application/JavaScript/index.js
+++ Application/JavaScript/index.js
@@ -6,22 +6,23 @@ $(function () { // Main Function
     var mainContainer = $('#main-container');
     // Top Section Variables
     var sectionTop = $('#section-top');
-    var widgetOne = sectionTop.children().eq(0); // Check the 0th child of sectionTop Div
-    var widgetTwo = sectionTop.children().eq(1);
-    var widgetThree = sectionTop.children().eq(2);
-    var widgetFour = sectionTop.children().eq(3);
+    var widgetTime = sectionTop.children().eq(0); // Check the 0th child of sectionTop Div
+    var widgetWeather = sectionTop.children().eq(1);
+    var widgetMap = sectionTop.children().eq(2);
+    var widgetNews = sectionTop.children().eq(3);
     // Bottom Section Variables
     var sectionBottom = $('#section-bottom');
     var hub = $('#hub');
 
-    hub.append(getTimeDiv());
-
+    annyang.trigger('show me the news of cnn');
 
     // Focuses or Unfocuses on a widget depending on number
     function focusWidget(number) {
+        clearFocus();
+        removeHubContents();
         switch (number) {
             case 1:
-                var widgetImg = widgetOne.children().first();
+                var widgetImg = widgetTime.children().first();
                 if (widgetImg.hasClass('bump-down')){
                     widgetImg.removeClass('bump-down');
                 }else {
@@ -29,7 +30,7 @@ $(function () { // Main Function
                 }
                 break;
             case 2:
-                var widgetImg = widgetTwo.children().first();
+                var widgetImg = widgetWeather.children().first();
                 if (widgetImg.hasClass('bump-down')){
                     widgetImg.removeClass('bump-down');
                 }else {
@@ -37,7 +38,7 @@ $(function () { // Main Function
                 }
                 break;
             case 3:
-                var widgetImg = widgetThree.children().first();
+                var widgetImg = widgetMap.children().first();
                 if (widgetImg.hasClass('bump-down')){
                     widgetImg.removeClass('bump-down');
                 }else {
@@ -45,7 +46,7 @@ $(function () { // Main Function
                 }
                 break;
             case 4:
-                var widgetImg = widgetFour.children().first();
+                var widgetImg = widgetNews.children().first();
                 if (widgetImg.hasClass('bump-down')){
                     widgetImg.removeClass('bump-down');
                 }else {
@@ -58,10 +59,10 @@ $(function () { // Main Function
     }
     // Removes focus from all widgets
     function clearFocus() {
-        widgetOne.children().first().removeClass('bump-down');
-        widgetTwo.children().first().removeClass('bump-down');
-        widgetThree.children().first().removeClass('bump-down');
-        widgetFour.children().first().removeClass('bump-down');
+        widgetTime.children().first().removeClass('bump-down');
+        widgetWeather.children().first().removeClass('bump-down');
+        widgetMap.children().first().removeClass('bump-down');
+        widgetNews.children().first().removeClass('bump-down');
     }
     // Removes everything from #hub
     function removeHubContents() {
@@ -79,6 +80,55 @@ $(function () { // Main Function
         }, 1000);
         return timeDiv;
     }
+
+    function getNews(sortedBy, dataSource) {
+        var newsSource = "";
+        if(dataSource != undefined){
+            switch (dataSource.toLowerCase()) {
+                case "cnn":
+                    newsSource = "cnn";
+                    break;
+                case "buzzfeed":
+                    newsSource = "buzzfeed";
+                    break;
+                case "cnbc":
+                    newsSource = "cnbc";
+                    break;
+                case "bloomberg":
+                    newsSource = "bloomberg";
+                    break;
+                default:
+                    newsSource = "cnn";
+            }
+        } else {
+            newsSource = "cnn"
+        }
+        if (sortedBy === undefined || sortedBy === "") {
+            sortedBy = 'top';
+        }
+        console.log(newsSource);
+        // AJAX call to get the news from newsSource sorted by sortedBy
+        $.ajax({
+            url: "https://newsapi.org/v1/articles?source=" + newsSource + "&sortBy=" + sortedBy + "&apiKey=0944a3e49a044c82b4f6c9473a25f0cf",
+            type: 'GET',
+            success: function(data){
+                console.log(data);
+            },
+            error: function(data) { // Something went wrong, probably source itsn't filterable by value of sortedBy
+                console.log(data.responseJSON.message + "\n-------------------\nGetting top news instead");
+                $.ajax({
+                    url: "https://newsapi.org/v1/articles?source=" + newsSource + "&sortBy=top&apiKey=0944a3e49a044c82b4f6c9473a25f0cf",
+                    type: 'GET',
+                    success: function(data){
+                        console.log(data);
+                    },
+                    error: function(data) {
+                        console.log(data);
+                    }
+                });
+            }
+        });
+    }
     // Starts the Speech to text
     function startListening() {
         if (annyang) {
@@ -86,26 +136,49 @@ $(function () { // Main Function
             var commands = {
                 'hi': function () { alert('Hello'); },
                 'show me the weather': function() {
-                    clearFocus();
                     focusWidget(2);
-                    removeHubContents();
                     $("#weather").css('visibility', 'visible');
                 },
                 'show me the time': function() {
-                    clearFocus();
                     focusWidget(1);
-                    removeHubContents();
                 },
                 'show me the map': function() {
-                    clearFocus();
                     focusWidget(3);
-                    removeHubContents();
                     $("#map").css('visibility', 'visible');
                 },
-                'show me the news': function() {
-                    clearFocus();
+                /**
+                Say, "Show me the most popular news of today from bloomberg"
+                Say, "What is the latest news from cnbc"
+                Say, "What is today's news (from buzzfeed)"
+                **/
+                '(what is) (what\'s) (show me) (the) (most) (:filter) (today\'s) (news) (story) (of) (today) (from) (:dataSource)': function(filter, dataSource) {
                     focusWidget(4);
-                    removeHubContents();
+                    var sortedBy = "";
+                    switch (filter.toLowerCase()) {
+                        case 'recent':
+                            console.log("Most recent news of: " + dataSource);
+                            sortedBy = 'latest';
+                            break;
+                        case 'today\'s':
+                            console.log("Todays news of: " + dataSource);
+                            sortedBy = 'latest';
+                            break;
+                        case 'latest':
+                            console.log("Latest news of: " + dataSource);
+                            sortedBy = 'latest';
+                            break;
+                        case 'top':
+                            console.log("Top news of: " + dataSource);
+                            sortedBy = 'top';
+                            break;
+                        case 'popular':
+                            console.log("Popular news of: " + dataSource);
+                            sortedBy = 'popular';
+                            break;
+                        default:
+
+                    }
+                    getNews(sortedBy, dataSource);
                 }
             };
             // Add our commands to annyang
